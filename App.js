ef44334f7929e65216a551c07b78acb801846098
GuilhermeJuan <guilhermelima156@outlook.com>
2023-11-06 00:13:48 -0300

    05/11

--- App.js
+++ App.js
@@ -14,10 +14,22 @@ export default function App() {
 }
 
 //Todas as blibliotecas usadas:
-//npm install @react-navigation/native
-//npm install react-native-web react-dom @expo/webpack-config
-//npm install react-native-screens react-native-safe-area-context
-//npm install @react-navigation/native-stack
-//npm install react-native-animatable
-//npm install react-native-calendars
+// npm install
+// npm install -g expo-cli
+// npm install @react-navigation/native
+// npm install react-native-web react-dom @expo/webpack-config
+// npm install react-native-screens react-native-safe-area-context
+// npm install @react-navigation/native-stack
+// npm install react-native-animatable
+// npm install react-native-calendars
+// npm install @react-navigation/bottom-tabs
+// npx expo install @expo/vector-icons
+// npm install react-native-modal
+// npx expo install expo-linear-gradient
+// npx expo install react-native-linear-gradient
+//ATUILIZAR PROJETO NO GIT:
+// git add .
+// git commit -a -m "..."
+// git push origin master
+
 
